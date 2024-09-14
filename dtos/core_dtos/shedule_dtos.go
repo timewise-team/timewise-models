52965d27fcc85a536fa8a1832064b5c3aff52d40
anhnguyenduc5 <anh.nguyenduc.work@gmail.com>
2024-09-15 01:26:28 +0700

    Update shedule_dtos.go

--- dtos/core_dtos/shedule_dtos.go
+++ dtos/core_dtos/shedule_dtos.go
@@ -2,6 +2,27 @@ package core_dtos
 
 import "time"
 
+type TwScheduleResponse struct {
+	ID                int       `json:"id"`
+	WorkspaceID       int       `json:"workspace_id"`
+	BoardColumnID     int       `json:"board_column_id"`
+	Title             string    `json:"title"`
+	Description       string    `json:"description"`
+	StartTime         time.Time `json:"start_time"`
+	EndTime           time.Time `json:"end_time"`
+	Location          string    `json:"location"`
+	CreatedBy         int       `json:"created_by"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
+	Status            string    `json:"status"`
+	AllDay            bool      `json:"all_day"`
+	Visibility        string    `json:"visibility"`
+	ExtraData         string    `json:"extra_data"`
+	IsDeleted         bool      `json:"is_deleted"`
+	RecurrencePattern string    `json:"recurrence_pattern"`
+	AssignedTo        []int     `json:"assigned_to"`
+}
+
 type TwCreateScheduleRequest struct {
 	WorkspaceID       *int       `json:"workspace_id,omitempty"`       // Nullable field
 	BoardColumnID     *int       `json:"board_column_id,omitempty"`    // Nullable field
