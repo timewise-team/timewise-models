5dcc8f9167be9d1cc46aae0fa1711af4e0b8b0db
Quang Thuan <quangthuan210103@gmail.com>
2024-09-18 20:27:47 +0700

    update register dto

---
+++ dtos/user_register_dtos/auth_dtos.go
@@ -0,0 +1,17 @@
+package core_dtos
+
+type RegisterRequestDto struct {
+	UserName        string `json:"username"`
+	FullName        string `json:"full_name"`
+	Email           string `json:"email"`
+	Password        string `json:"password"`
+	ConfirmPassword string `json:"confirm_password"`
+}
+
+type RegisterResponseDto struct {
+	UserName     string `json:"username"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Email        string `json:"email"`
+	HashPassword string `json:"hash_password"`
+}
