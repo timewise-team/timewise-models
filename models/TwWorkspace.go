a31a42232a17bc855c83bd18b225d1207eaebd66
Builanviet <builanviet@gmail.com>
2024-10-02 22:25:57 +0700

    fix model

--- models/TwWorkspace.go
+++ models/TwWorkspace.go
@@ -5,15 +5,16 @@ import (
 )
 
 type TwWorkspace struct {
-	ID          int       `gorm:"primary_key"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	DeletedAt   time.Time `json:"deleted_at" gorm:"default:null"`
-	Title       string    `json:"title"`
-	ExtraData   string    `json:"extra_data"`
-	Description string    `json:"description"`
-	CreatedBy   int       `json:"created_by"`
-	Key         string    `json:"key"`
-	Type        string    `json:"type"`
-	IsDeleted   bool      `json:"is_deleted"`
+	ID          int         `gorm:"primary_key"`
+	CreatedAt   time.Time   `json:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at"`
+	DeletedAt   time.Time   `json:"deleted_at" gorm:"default:null"`
+	Title       string      `json:"title"`
+	ExtraData   string      `json:"extra_data"`
+	Description string      `json:"description"`
+	CreatedBy   int         `json:"created_by"`
+	Key         string      `json:"key"`
+	Type        string      `json:"type"`
+	IsDeleted   bool        `json:"is_deleted"`
+	CreateBy    TwUserEmail `gorm:"foreignkey:CreatedBy;association_foreignkey:ID"`
 }
