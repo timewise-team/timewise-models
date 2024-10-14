54429168c515a4b5dfe1b0db63cde0d6ed0ccce4
Builanviet <builanviet@gmail.com>
2024-10-14 23:34:32 +0700

    add schedule_participant dtos

---
+++ dtos/core_dtos/schedule_participant_dtos/schedule_participant_response.go
@@ -0,0 +1,21 @@
+package schedule_participant_dtos
+
+type ScheduleParticipantInfo struct {
+	Id                  int    `json:"id"`
+	ScheduleId          int    `json:"schedule_id"`
+	WorkspaceUserId     int    `json:"workspace_user_id"`
+	Status              string `json:"status"`
+	AssignAt            string `json:"assign_at"`
+	AssignBy            int    `json:"assign_by"`
+	ResponseTime        string `json:"response_time"`
+	InvitationSentAt    string `json:"invitation_sent_at"`
+	InvitationStatus    string `json:"invitation_status"`
+	Role                string `json:"role"`
+	StatusWorkspaceUser string `json:"status_workspace_user"`
+	IsVerified          bool   `json:"is_verified"`
+	UserId              int    `json:"user_id" `
+	Email               string `json:"email"`
+	FirstName           string `json:"first_name"`
+	LastName            string `json:"last_name"`
+	ProfilePicture      string `json:"profile_picture"`
+}
