00b62aa6d4926e46ac75eaa3f7e485da9118ad03
QuangThuan21 <quangthuan210103@gmail.com>
2024-10-22 17:01:28 +0700

    update create schedule response

--- dtos/core_dtos/shedule_dtos.go
+++ dtos/core_dtos/shedule_dtos.go
@@ -50,26 +50,11 @@ type TwCreateScheduleRequest struct {
 }
 
 type TwCreateShecduleResponse struct {
-	ID                int       `json:"id"`
-	WorkspaceID       int       `json:"workspace_id"`
-	BoardColumnID     int       `json:"board_column_id"`
-	Title             string    `json:"title"`
-	Description       string    `json:"description"`
-	StartTime         time.Time `json:"start_time"`
-	EndTime           time.Time `json:"end_time"`
-	Location          string    `json:"location"`
-	CreatedBy         int       `json:"created_by"`
-	CreatedAt         time.Time `json:"created_at"`
-	UpdatedAt         time.Time `json:"updated_at"`
-	Status            string    `json:"status"`
-	AllDay            bool      `json:"all_day"`
-	Visibility        string    `json:"visibility"`
-	VideoTranscript   string    `json:"video_transcript"`
-	ExtraData         string    `json:"extra_data,omitempty"`
-	IsDeleted         bool      `json:"is_deleted"`
-	RecurrencePattern string    `json:"recurrence_pattern"`
-	Position          int       `json:"position"`
-	Priority          string    `json:"priority"`
+	ID            int    `json:"id"`
+	WorkspaceID   int    `json:"workspace_id"`
+	BoardColumnID int    `json:"board_column_id"`
+	Title         string `json:"title"`
+	Position      int    `json:"position"`
 }
 
 type TwUpdateScheduleRequest struct {
