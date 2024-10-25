fa2266323da5b81a36e5163a7c880554388c2bdc
QuangThuan21 <quangthuan210103@gmail.com>
2024-10-25 19:43:41 +0700

    update schedule participant model again v2

--- models/TwScheduleParticipant.go
+++ models/TwScheduleParticipant.go
@@ -12,10 +12,10 @@ type TwScheduleParticipant struct {
 	ScheduleId       int             `json:"schedule_id" gorm:"index"`
 	WorkspaceUserId  int             `json:"workspace_user_id" gorm:"index"`
 	Status           string          `json:"status"`
-	AssignAt         time.Time       `json:"assign_at" gorm:"default:null"`
+	AssignAt         *time.Time      `json:"assign_at" gorm:"default:null"`
 	AssignBy         int             `json:"assign_by"`
-	ResponseTime     time.Time       `json:"response_time" gorm:"default:null"`
-	InvitationSentAt time.Time       `json:"invitation_sent_at" gorm:"default:null"`
+	ResponseTime     *time.Time      `json:"response_time" gorm:"default:null"`
+	InvitationSentAt *time.Time      `json:"invitation_sent_at" gorm:"default:null"`
 	InvitationStatus string          `json:"invitation_status"`
 	Schedule         TwSchedule      `gorm:"foreignkey:ScheduleId;association_foreignkey:ID"`
 	WorkspaceUser    TwWorkspaceUser `gorm:"foreignkey:WorkspaceUserId;association_foreignkey:ID"`
