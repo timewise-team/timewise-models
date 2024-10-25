72e70c3d2b8a1bda493c8ff9001b094c8e8d1afd
QuangThuan21 <quangthuan210103@gmail.com>
2024-10-25 16:50:22 +0700

    update schedule participant

--- models/TwScheduleParticipant.go
+++ models/TwScheduleParticipant.go
@@ -6,16 +6,16 @@ import (
 
 type TwScheduleParticipant struct {
 	ID               int             `gorm:"primary_key"`
-	CreatedAt        time.Time       `json:"created_at"`
-	UpdatedAt        time.Time       `json:"updated_at"`
-	DeletedAt        time.Time       `json:"deleted_at" gorm:"default:null"`
+	CreatedAt        *time.Time      `json:"created_at"`
+	UpdatedAt        *time.Time      `json:"updated_at"`
+	DeletedAt        *time.Time      `json:"deleted_at" gorm:"default:null"`
 	ScheduleId       int             `json:"schedule_id" gorm:"index"`
 	WorkspaceUserId  int             `json:"workspace_user_id" gorm:"index"`
 	Status           string          `json:"status"`
-	AssignAt         time.Time       `json:"assign_at" gorm:"default:null"`
+	AssignAt         *time.Time      `json:"assign_at" gorm:"default:null"`
 	AssignBy         int             `json:"assign_by"`
-	ResponseTime     time.Time       `json:"response_time" gorm:"default:null"`
-	InvitationSentAt time.Time       `json:"invitation_sent_at" gorm:"default:null"`
+	ResponseTime     *time.Time      `json:"response_time" gorm:"default:null"`
+	InvitationSentAt *time.Time      `json:"invitation_sent_at" gorm:"default:null"`
 	InvitationStatus string          `json:"invitation_status"`
 	Schedule         TwSchedule      `gorm:"foreignkey:ScheduleId;association_foreignkey:ID"`
 	WorkspaceUser    TwWorkspaceUser `gorm:"foreignkey:WorkspaceUserId;association_foreignkey:ID"`
