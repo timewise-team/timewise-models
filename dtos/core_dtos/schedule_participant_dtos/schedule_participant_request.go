0104bbc5923385bdd6525f67cf22076ab7b1aa22
QuangThuan21 <quangthuan210103@gmail.com>
2024-10-28 08:27:26 +0700

    schedule participant dtos

--- dtos/core_dtos/schedule_participant_dtos/schedule_participant_request.go
+++ dtos/core_dtos/schedule_participant_dtos/schedule_participant_request.go
@@ -1,6 +1,17 @@
 package schedule_participant_dtos
 
+import "time"
+
 type InviteToScheduleRequest struct {
 	Email      string `json:"email"`
 	ScheduleId int    `json:"schedule_id"`
+}
+
+type ScheduleParticipantRequest struct {
+	Status           *string    `json:"status"`
+	AssignAt         *time.Time `json:"assign_at" gorm:"default:null"`
+	AssignBy         *int       `json:"assign_by"`
+	ResponseTime     *time.Time `json:"response_time" gorm:"default:null"`
+	InvitationSentAt *time.Time `json:"invitation_sent_at" gorm:"default:null"`
+	InvitationStatus *string    `json:"invitation_status"`
 }
