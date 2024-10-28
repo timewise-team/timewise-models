deefa01a9ea4939a9e2be450aecc786c4374bfcf
QuangThuan21 <quangthuan210103@gmail.com>
2024-10-28 08:38:22 +0700

    update schedule participant dto v2

--- dtos/core_dtos/schedule_participant_dtos/schedule_participant_response.go
+++ dtos/core_dtos/schedule_participant_dtos/schedule_participant_response.go
@@ -23,13 +23,13 @@ type ScheduleParticipantInfo struct {
 }
 
 type ScheduleParticipantResponse struct {
-	ID               int       `gorm:"primary_key"`
-	ScheduleId       int       `json:"schedule_id" gorm:"index"`
-	WorkspaceUserId  int       `json:"workspace_user_id" gorm:"index"`
-	Status           string    `json:"status"`
-	AssignAt         time.Time `json:"assign_at" gorm:"default:null"`
-	AssignBy         int       `json:"assign_by"`
-	ResponseTime     time.Time `json:"response_time" gorm:"default:null"`
-	InvitationSentAt time.Time `json:"invitation_sent_at" gorm:"default:null"`
-	InvitationStatus string    `json:"invitation_status"`
+	ID               int        `gorm:"primary_key"`
+	ScheduleId       int        `json:"schedule_id" gorm:"index"`
+	WorkspaceUserId  int        `json:"workspace_user_id" gorm:"index"`
+	Status           string     `json:"status"`
+	AssignAt         *time.Time `json:"assign_at" gorm:"default:null"`
+	AssignBy         int        `json:"assign_by"`
+	ResponseTime     *time.Time `json:"response_time" gorm:"default:null"`
+	InvitationSentAt *time.Time `json:"invitation_sent_at" gorm:"default:null"`
+	InvitationStatus string     `json:"invitation_status"`
 }
