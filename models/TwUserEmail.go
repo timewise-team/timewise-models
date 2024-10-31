3937fdfba5fcb0b1295913914b489464a5a15b17
anh.nguyenduc4 <anh.nguyenduc.work@gmail.com>
2024-10-31 14:20:37 +0700

    Update TwUserEmail.go

--- models/TwUserEmail.go
+++ models/TwUserEmail.go
@@ -5,11 +5,11 @@ import (
 )
 
 type TwUserEmail struct {
-	ID        int       `gorm:"primary_key"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at" gorm:"default:null"`
-	UserId    int       `json:"user_id" gorm:"index"`
-	Email     string    `json:"email"`
-	User      TwUser    `gorm:"foreignkey:UserId;association_foreignkey:ID"`
+	ID        int        `gorm:"primary_key"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at" gorm:"default:null"`
+	UserId    int        `json:"user_id" gorm:"index"`
+	Email     string     `json:"email"`
+	User      TwUser     `gorm:"foreignkey:UserId;association_foreignkey:ID"`
 }
