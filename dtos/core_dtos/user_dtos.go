897fca3f21bf519a04feade3926a2618566e336f
anh.nguyenduc4 <anh.nguyenduc.work@gmail.com>
2024-11-01 13:41:08 +0700

    Update user_dtos.go

--- dtos/core_dtos/user_dtos.go
+++ dtos/core_dtos/user_dtos.go
@@ -5,18 +5,18 @@ import (
 )
 
 type UpdateUserRequest struct {
-	FirstName            string    `gorm:"type:varchar(100)"`
-	LastName             string    `gorm:"type:varchar(100)"`
-	Email                string    `gorm:"type:varchar(255)"`
-	ProfilePicture       string    `gorm:"type:varchar(255)"`
-	Timezone             string    `gorm:"type:varchar(100)"`
-	Locale               string    `gorm:"type:varchar(10)"`
-	IsVerified           bool      `gorm:"type:tinyint(1)"`
-	IsActive             bool      `gorm:"type:tinyint(1)"`
+	FirstName            *string   `gorm:"type:varchar(100)"`
+	LastName             *string   `gorm:"type:varchar(100)"`
+	Email                *string   `gorm:"type:varchar(255)"`
+	ProfilePicture       *string   `gorm:"type:varchar(255)"`
+	Timezone             *string   `gorm:"type:varchar(100)"`
+	Locale               *string   `gorm:"type:varchar(10)"`
+	IsVerified           *bool     `gorm:"type:tinyint(1)"`
+	IsActive             *bool     `gorm:"type:tinyint(1)"`
 	CreatedAt            time.Time `gorm:"type:datetime"`
 	UpdatedAt            time.Time `gorm:"type:datetime"`
 	LastLoginAt          time.Time `gorm:"type:datetime"`
-	NotificationSettings string    `gorm:"type:text"`
-	CalendarSettings     string    `gorm:"type:text"`
-	Role                 string    `gorm:"type:varchar(50)"`
+	NotificationSettings *string   `gorm:"type:text"`
+	CalendarSettings     *string   `gorm:"type:text"`
+	Role                 *string   `gorm:"type:varchar(50)"`
 }
