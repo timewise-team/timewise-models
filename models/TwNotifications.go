228a44a935a23b4b4b60c6c72318ae1196167acd
Khanh Hoang <khanhhn@metric.vn>
2024-11-05 22:02:51 +0700

    update TwNotifications

--- models/TwNotifications.go
+++ models/TwNotifications.go
@@ -16,4 +16,5 @@ type TwNotifications struct {
 	ExtraData       string      `json:"extra_data"`
 	UserEmail       TwUserEmail `gorm:"foreignkey:UserEmailId ;association_foreignkey:ID"`
 	IsSent          bool        `json:"is_sent" gorm:"default:false"`
+	NotifiedAt      *time.Time  `json:"notified_at" gorm:"default:now()"`
 }
