c3dd7096076622d483bb34da9b53e16ea7659b52
QuangThuan21 <quangthuan210103@gmail.com>
2024-11-07 15:36:50 +0700

    user_email dto

--- dtos/core_dtos/user_email_dtos/user_email_response.go
+++ dtos/core_dtos/user_email_dtos/user_email_response.go
@@ -7,3 +7,13 @@ type SearchUserEmailResponse struct {
 	LastName       string `json:"last_name"`
 	ProfilePicture string `json:"profile_picture"`
 }
+
+type UserEmailStatusResponse struct {
+	Id             int    `json:"id"`
+	Email          string `json:"email"`
+	FirstName      string `json:"first_name"`
+	LastName       string `json:"last_name"`
+	ProfilePicture string `json:"profile_picture"`
+	Status         string `json:"status"`
+	IsVerified     bool   `json:"is_verified"`
+}
