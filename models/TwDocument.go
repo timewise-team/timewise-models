b160b237db7366dab48fe12ef73d0174abbb12cc
anh.nguyenduc4 <anh.nguyenduc.work@gmail.com>
2024-11-07 15:45:29 +0700

    Update TwDocument.go

--- models/TwDocument.go
+++ models/TwDocument.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type TwDocument struct {
 	ID            int             `gorm:"primary_key" `
 	FileName      string          `gorm:"type:varchar(255)" json:"file_name"`
@@ -9,10 +11,10 @@ type TwDocument struct {
 	IsDeleted     bool            `gorm:"type:tinyint(1)" json:"is_deleted"`
 	ScheduleId    int             `gorm:"index" json:"schedule_id"`
 	UploadedBy    int             `gorm:"index" json:"workspace_user_id"`
-	CreatedAt     string          `gorm:"type:datetime" json:"created_at"`
-	UpdatedAt     string          `gorm:"type:datetime" json:"updated_at"`
-	DeletedAt     string          `gorm:"type:datetime" gorm:"default:null" json:"deleted_at"`
-	UploadedAt    string          `gorm:"type:datetime" json:"uploaded_at"`
+	CreatedAt     time.Time       `json:"created_at"`
+	UpdatedAt     time.Time       `json:"updated_at"`
+	DeletedAt     *time.Time      `gorm:"default:null" json:"deleted_at"`
+	UploadedAt    time.Time       `json:"uploaded_at"`
 	DownloadUrl   string          `gorm:"type:varchar(255)"json:"download_url"`
 	WorkspaceUser TwWorkspaceUser `gorm:"foreignkey:UploadedBy;association_foreignkey:ID"`
 	Schedule      TwSchedule      `gorm:"foreignkey:ScheduleId;association_foreignkey:ID"`
