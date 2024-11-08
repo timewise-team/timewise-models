aa688d85a98cf6420f65028bc7890fb2dc3a7085
anh.nguyenduc4 <anh.nguyenduc.work@gmail.com>
2024-11-08 10:32:14 +0700

    Update account_dtos.go

--- dtos/core_dtos/account_dtos.go
+++ dtos/core_dtos/account_dtos.go
@@ -3,23 +3,28 @@ package core_dtos
 import "time"
 
 type GetUserResponseDto struct {
-	ID                   int       `json:"id"`
-	CreatedAt            time.Time `json:"created_at"`
-	UpdatedAt            time.Time `json:"updated_at"`
-	DeteledAt            time.Time `json:"deleted_at"`
-	FirstName            string    `json:"first_name"`
-	LastName             string    `json:"last_name"`
-	Email                []string  `json:"email"`
-	ProfilePicture       string    `json:"profile_picture"`
-	Timezone             string    `json:"timezone"`
-	Locale               string    `json:"locale"`
-	GoogleId             string    `json:"google_id"`
-	IsVerified           bool      `json:"is_verified"`
-	IsActive             bool      `json:"is_active"`
-	LastLoginAt          time.Time `json:"last_login_at"`
-	NotificationSettings string    `json:"notification_settings"`
-	CalendarSettings     string    `json:"calendar_settings"`
-	Role                 string    `json:"role"`
+	ID                   int        `json:"id"`
+	CreatedAt            time.Time  `json:"created_at"`
+	UpdatedAt            time.Time  `json:"updated_at"`
+	DeteledAt            time.Time  `json:"deleted_at"`
+	FirstName            string     `json:"first_name"`
+	LastName             string     `json:"last_name"`
+	Email                []EmailDto `json:"email"`
+	ProfilePicture       string     `json:"profile_picture"`
+	Timezone             string     `json:"timezone"`
+	Locale               string     `json:"locale"`
+	GoogleId             string     `json:"google_id"`
+	IsVerified           bool       `json:"is_verified"`
+	IsActive             bool       `json:"is_active"`
+	LastLoginAt          time.Time  `json:"last_login_at"`
+	NotificationSettings string     `json:"notification_settings"`
+	CalendarSettings     string     `json:"calendar_settings"`
+	Role                 string     `json:"role"`
+}
+
+type EmailDto struct {
+	Email  string `json:"email"`
+	Status string `json:"status"`
 }
 
 type UpdateProfileRequestDto struct {
