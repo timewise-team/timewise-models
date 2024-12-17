5d1952d34d8f57281d67a1bb57596ae3a0bfb328
QuangThuan21 <quangthuan210103@gmail.com>
2024-12-17 11:54:21 +0700

    update

--- dtos/core_dtos/workspace_user_dtos/workspace_user_response.go
+++ dtos/core_dtos/workspace_user_dtos/workspace_user_response.go
@@ -20,3 +20,23 @@ type GetWorkspaceUserListResponse struct {
 	LastName       string    `json:"last_name"`
 	ProfilePicture string    `json:"profile_picture"`
 }
+
+type GetWorkspaceUserListNotVerifiedResponse struct {
+	ID             int       `json:"id"`
+	UserEmailId    int       `json:"user_email_id"`
+	WorkspaceId    int       `json:"workspace_id"` // Sẽ gán giá trị của schedule_id
+	WorkspaceKey   string    `json:"workspace_key"`
+	Role           string    `json:"role"`
+	Status         string    `json:"status"`
+	IsActive       bool      `json:"is_active"`
+	IsVerified     bool      `json:"is_verified"`
+	ExtraData      string    `json:"extra_data"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	DeletedAt      time.Time `json:"deleted_at"`
+	Email          string    `json:"email"`
+	FirstName      string    `json:"first_name"`
+	LastName       string    `json:"last_name"`
+	ProfilePicture string    `json:"profile_picture"`
+	ScheduleId     int       `json:"schedule_id"`
+}
